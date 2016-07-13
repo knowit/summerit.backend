0e25029491483af3de1f46b1e250062842101285
aulonm <aulon.mujaj@gmail.com>
2016-07-13 16:46:56 +0200

    deleted stuff

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "summerit.backend",
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 20),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 0, minor: 9),
-    ]
-)
