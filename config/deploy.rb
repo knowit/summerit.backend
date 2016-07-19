38dd7281ed3a3f0be51e1078a9cea59462ee714d
Filip Egge <filip.egge@gmail.com>
2016-07-19 19:25:02 +0200

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :passenger_restart_with_touch, true
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+:linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
